4bf00fa836b13255cdd155791d61d96a3984b5fe
alexbaldwin42 <alexbaldwin42@gmail.com>
2023-05-04 07:57:45 -0600

    updated user keys and added auto primary selection

--- keyboards/bastardkb/charybdis/3x5_3/v2/splinky_3/config.h
+++ keyboards/bastardkb/charybdis/3x5_3/v2/splinky_3/config.h
@@ -24,12 +24,12 @@
     { GP28, GP21, GP6, GP7, GP8 }
 
 /* Handedness. */
-#define MASTER_RIGHT
+//#define MASTER_RIGHT
 
 // To use the handedness pin, resistors need to be installed on the adapter PCB.
 // If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP15
-// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
+ #define SPLIT_HAND_PIN GP15
+ #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
 
 /* serial.c configuration (for split keyboard). */
 #define SOFT_SERIAL_PIN GP1
